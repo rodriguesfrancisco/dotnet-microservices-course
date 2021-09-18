0851fe033a7ddac308514511f0da8638cdf2d550
Francisco Victor Rodrigues <f.co.victor@hotmail.com>
2021-09-18 13:03:17 -0300

    Add first service

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,60 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product(
+                    "IPhone X", 
+                    "Smart Phone",
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    "product-1.png",
+                    950.00M
+                ),
+                new Product(
+                    "IPhone 11",
+                    "Smart Phone",
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    "product-2.png",
+                    1050.00M
+                ),
+                new Product(
+                    "IPhone 12",
+                    "Smart Phone",
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    "product-3.png",
+                    1150.00M
+                ),
+                new Product(
+                    "IPhone 13",
+                    "Smart Phone",
+                    "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
+                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    "product-4.png",
+                    1250.00M
+                ),
+            };
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        public Product(string name, string category, string summary, string description, string imageFile, decimal price)
+        {
+            Name = name;
+            Category = category;
+            Summary = summary;
+            Description = description;
+            ImageFile = imageFile;
+            Price = price;
+        }
+        protected Product() { }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Category { get; private set; }
+        public string Summary { get; private set; }
+        public string Description { get; private set; }
+        public string ImageFile { get; private set; }
+        public decimal Price { get; private set; }
+    }
+}

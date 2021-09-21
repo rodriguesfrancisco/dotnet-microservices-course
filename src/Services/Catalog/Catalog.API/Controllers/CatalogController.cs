51d7880459460f72b1b5ae70767977e558e35297
Francisco Rodrigues <f.co.victor@hotmail.com>
2021-09-21 16:26:29 -0300

    Finish catalog service

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -35,7 +35,48 @@ namespace Catalog.API.Controllers
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetProductById(string id)
         {
+            var product = await _productRepository.GetProductAsync(id);
 
+            if(product == null)
+            {
+                _logger.LogError($"Product with id {id} not found");
+                return NotFound();
+            }
+
+            return Ok(product);
         }
+        
+        [Route("by-category/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetProductByCategory(string category)
+        {
+            var products = await _productRepository.GetProductsByCategoryAsync(category);
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CreateProduct([FromBody] Product product)
+        {
+            await _productRepository.CreateProductAsync(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _productRepository.UpdateProductAsync(product));
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _productRepository.DeleteProductAsync(id));
+        }
+        
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,4 @@
+using Catalog.API.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
